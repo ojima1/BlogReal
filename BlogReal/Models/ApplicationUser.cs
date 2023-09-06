d1fcf65e871330dd98eddf922a1f28d782bea603
Utens <Utens@UTENS>
2023-09-06 22:42:28 +0100

    Add project files.

---
+++ BlogReal/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlogReal.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public String? FirstName { get; set; }
+        public string? LastName { get; set;}
+        //relation
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ BlogReal/Models/Page.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogReal.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public String? ShortDescription { get; set; }
+        public String? Description { get; set;}
+        public String Slug { get; set; }
+    }
+}
